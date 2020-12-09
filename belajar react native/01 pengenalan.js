0fb74bdd98720804b3ed8c965a513afe95bd0548
Agus Priyatin <aguspriyatin80@gmail.com>
2020-12-09 11:02:55 +0700

    belajar-react-native

---
+++ belajar react native/01 pengenalan.js
@@ -0,0 +1,34 @@
+/*
+01 PENGENALAN
+-------------
+React Native : 
+1) adalah javascript framework untuk membuat aplikasi mobile
+lintas platform iOS dan Android
+2) dibuat oleh facebook menggunakan library React
+3) pertama kali di release tahun 2015
+4) motto RN: learn once, write anywhere
+5) dibuat dengan javascript library namun dirender dalam mobile sebagai native apps
+
+02 PERSIAPAN ENVIRONMENT
+1) install nodejs
+2) install git
+3) android studio 
+4) text editor ex: visual studio code (beserta extension yang diperlukan)
+5) cek dokumentasi :
+   - setting up environment
+   - React Native CLI Quickstart
+   - install node
+   - install watchman 
+   - instal java development kit (jdk)   
+   - install Android SDK
+   - install emulator
+   - konfigurasi android studio :
+        export ANDROID_HOME=$HOME/Android/Sdk
+        export PATH=$PATH:$ANDROID_HOME/emulator
+        export PATH=$PATH:$ANDROID_HOME/tools
+        export PATH=$PATH:$ANDROID_HOME/tools/bin
+        export PATH=$PATH:$ANDROID_HOME/platform-tools
+        
+
+
+*/
